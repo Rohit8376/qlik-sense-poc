b25bdae338883ec20500d3e24f11bc9dc9dff6a2
Rohit Pandey <rohit.pandey@exponentia.ai>
2023-03-03 17:19:56 +0530

    d

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const cors = require('cors')
 var cookieParser = require('cookie-parser');
 var bodyparser = require('body-parser')
+const request = require('request')
+
 
 var app = express();
 app.use(express.json())
@@ -11,81 +13,95 @@ app.use(cors())
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '../public'))) 
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(bodyparser.urlencoded({
     extended: true
-  }));
+}));
 
 // app.get('/', (req,res)=>{
 //     res.render('sso')
 // })
 
 app.get('/', function (req, res) {
-    if(!req.cookies.isloggedin){ 
+    if (!req.cookies.isloggedin) {
         res.redirect('/login')
         return
     }
-    console.log("cookey",req.cookies.userId)
-    res.render('app', {userId:req.cookies.userId})
-}); 
+    console.log("cookey", req.cookies.userId)
+    res.render('app', { userId: req.cookies.userId })
+});
 
 app.get('/app2', function (req, res) {
-    if(!req.cookies.isloggedin){ 
+    if (!req.cookies.isloggedin) {
         res.redirect('/login')
         return
     }
-    console.log("cookey",req.cookies.userId)
-    res.render('app2', {userId:req.cookies.userId})
+    console.log("cookey", req.cookies.userId)
+    res.render('app2', { userId: req.cookies.userId })
 });
 
 
-app.get('/single-object',(req,res)=>{
-    if(!req.cookies.isloggedin){
+app.get('/single-object', (req, res) => {
+    if (!req.cookies.isloggedin) {
         res.redirect('/login')
         return
     }
-    res.render('single-object',{userId:req.cookies.userId})
+    res.render('single-object', { userId: req.cookies.userId })
 })
 
-app.get('/single-sheet',(req,res)=>{
-    if(!req.cookies.isloggedin){
+app.get('/single-sheet', (req, res) => {
+    if (!req.cookies.isloggedin) {
         res.redirect('/login')
         return
     }
-    res.render('single-sheet',{userId:req.cookies.userId})
+    res.render('single-sheet', { userId: req.cookies.userId })
 })
 
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login',(req,res)=>{
+app.post('/login', (req, res) => {
     console.log(req.body)
-    res.cookie(`isloggedin`,true);
-    res.cookie(`userId`,req.body.userId);
-    res.redirect('/') 
+    res.cookie(`isloggedin`, true);
+    res.cookie(`userId`, req.body.userId);
+    res.redirect('/')
 })
 
-app.post('/logout',(req,res)=>{
-    res.clearCookie('isloggedin')
-    res.clearCookie('userId')
-    res.cookie(`isloggedin`,false);
-    res.cookie(`userId`,"");
-    console.log(req.cookies.userId)
-    console.log(req.cookies.isloggedin)
-    res.redirect('/login') 
-}) 
+app.post('/logout', async (req, res) => {
+
+
+    let url = "https://exponentia-ai-cloud.ap.qlikcloud.com/logout";
+    request(url, (error, response, body) => {
+        // Printing the error if occurred
+        if (error) console.log(error)
+        // Printing status code
+        console.log(response.statusCode);
+        // Printing body
+        console.log(body);
+    });
+
+    // res.clearCookie('isloggedin')
+    // res.clearCookie('userId')
+    // res.cookie(`isloggedin`,false);
+    // res.cookie(`userId`,"");
+    // console.log(req.cookies.userId)
+    // console.log(req.cookies.isloggedin)
+    // res.redirect('/login') 
+})
 
 
 
 
-app.get('/vidaltesting', (req,res)=>{
+app.get('/vidaltesting', (req, res) => {
     res.render('index3')
 })
 
-let port =process.env.PORT || 3000
+let port = process.env.PORT || 3000
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Listening on port 3000");
-});
+})
+
+
